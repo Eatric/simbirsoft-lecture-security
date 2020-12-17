78965776a2006f9f659f4702b843c2dfdc37b0de
Eatric <iksanovk@gmail.com>
2020-12-17 16:05:35 +0300

    Initial commit

---
+++ 1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TestSimbirsoftVulnerabilities
+{
+	class Program
+	{
+		public static string Salt()
+		{
+			var stringBuilder = new StringBuilder();
+			var rand = new Random();
+
+			for (var i = 0; i < 8; i++)
+			{
+				var isBigSymbol = rand.Next(0, 2);
+				var symbol = rand.Next(0, 26);
+
+				stringBuilder.Append((char)(
+					isBigSymbol == 1 ? (65 + symbol) : (97 + symbol)));
+			}
+
+			return stringBuilder.ToString();
+		}
+
+		public static string PasswordHash(string data, string salt)
+		{
+			var strResult = string.Empty;
+
+			using (var md5 = new MD5CryptoServiceProvider())
+			{
+				var hashData = Encoding.UTF8.GetBytes(data + salt);
+				var hashDataEnc = md5.ComputeHash(hashData);
+
+				strResult = Convert.ToBase64String(hashDataEnc);
+			}
+
+			return strResult;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter password: ");
+			var password = Console.ReadLine();
+			var salt = Salt();
+			var hashResult = PasswordHash(password, salt);
+			Console.WriteLine($"Salt: {salt}");
+			Console.WriteLine($"PasswordHash: {hashResult}");
+		}
+	}
+}
---
+++ 3.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TestSimbirsoftVulnerabilities
+{
+	class Program3
+	{
+		public static string Salt()
+		{
+			byte[] salt = new byte[256 / 8];
+			using (var rng = RandomNumberGenerator.Create())
+			{
+				rng.GetBytes(salt);
+			}
+
+			return Convert.ToBase64String(salt);
+		}
+
+		public static string PasswordHash(string pwd, string salt, int iterations)
+		{
+			var strResult = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+				password: pwd,
+				salt: Convert.FromBase64String(salt),
+				prf: KeyDerivationPrf.HMACSHA256,
+				iterationCount: iterations,
+				numBytesRequested: 512 / 8));
+
+			return strResult;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter password: ");
+			var password = Console.ReadLine();
+			var salt = Salt();
+			var hashResult = PasswordHash(password, salt, 10000);
+			Console.WriteLine($"Salt: {salt}");
+			Console.WriteLine($"PasswordHash: {hashResult}");
+		}
+	}
+}
